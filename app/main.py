47bfb762b8ff931e47c6c2d1cfb710bb9f00039b
Ming <wmj327728951@gmail.com>
2026-03-02 09:40:30 +0100

    New solution

--- app/main.py
+++ app/main.py
@@ -5,12 +5,13 @@ from app.people.customer import Customer
 
 
 def cinema_visit(customers: list[dict]
-                 , hall_number: int, cleaner: str, movie: str) -> None:
+                 , hall_number: int, cleaner: str
+                 , movie: str) -> None:
     cinema_hall = CinemaHall(hall_number)
     cleaner_obj = Cleaner(cleaner)
     customer_objects = []
     for customer_dict in customers:
         customer_obj = Customer(customer_dict["name"], customer_dict["food"])
-        CinemaBar.sell_product(customer_obj, customer_dict["food"])
+        CinemaBar.sell_product(customer_dict["food"], customer_obj)
         customer_objects.append(customer_obj)
     cinema_hall.movie_session(movie, customer_objects, cleaner_obj)
